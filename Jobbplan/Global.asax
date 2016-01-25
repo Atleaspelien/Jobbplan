da294ff5cc89a26abb3638e928a21965a040b345
mats løkken <mats_loekken@hotmail.com>
2016-01-25 11:45:00 +0100

    Start

---
+++ Jobbplan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jobbplan.Global" Language="C#" %>
---
+++ Jobbplan/Models/Bruker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jobbplan.Models
+{
+    public class Bruker
+    {
+        public class LogInn
+        {
+            [Required(ErrorMessage = "Du må skrive brukernavn")]
+            [Display(Name = "Brukernavn")]
+            public string Brukernavn { get; set; }
+
+            [Required(ErrorMessage = "Du må skrive inn passord!")]
+            [DataType(DataType.Password)]
+            [Display(Name = "Passord")]
+            public string Passord { get; set; }
+
+        }
+    }
+}
